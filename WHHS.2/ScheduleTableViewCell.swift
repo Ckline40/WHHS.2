44cf7a95d4d6e8e3e479b511dd2e606dcafca0fd
atnardo <atnardo@gmail.com>
2018-10-19 10:06:40 -0400

    alerts and text whoa

---
+++ WHHS.2/ScheduleTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  ScheduleTableViewCell.swift
+//  WHHS.2
+//
+//  Created by  on 10/19/18.
+//  Copyright © 2018 Christopher Kline. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleTableViewCell: UITableViewCell {
+
+   
+   
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func buttonPopup(_ sender: UIButton) { UIAlertController(title: "DJ", message: "bio", preferredStyle: .alert)
+        let action1 = UIAlertAction(title: "Default", style: .default) { (action:UIAlertAction) in
+            print("You've pressed default");
+                self.present(alertController, animated: true, completion: nil)
+    }
+//    let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
+//    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+//    NSLog("The \"OK\" alert occured.")
+//    }))
+//    self.present(alert, animated: true, completion: nil)
+//
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
